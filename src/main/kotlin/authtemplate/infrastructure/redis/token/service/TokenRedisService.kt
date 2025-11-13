f4e5e7042bf41505b70910d510edcffb6204551d
socury <socury1104@gmail.com>
2025-11-13 10:50:02 +0900

    refactor: 하드코딩 제거

--- src/main/kotlin/authtemplate/infrastructure/redis/token/service/TokenRedisService.kt
+++ src/main/kotlin/authtemplate/infrastructure/redis/token/service/TokenRedisService.kt
@@ -3,14 +3,16 @@ package authtemplate.infrastructure.redis.token.service
 import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Service
 import authtemplate.infrastructure.security.exception.RefreshTokenNotMatchException
+import authtemplate.infrastructure.security.token.properties.TokenProperties
 import java.util.concurrent.TimeUnit
 
 @Service
 class TokenRedisService (
     private val redisTemplate: StringRedisTemplate,
+    private val tokenProperties: TokenProperties
 ) {
     fun storeRefreshToken(key: String, refreshToken: String) {
-        redisTemplate.opsForValue().set("refresh_token:$key", refreshToken, 1, TimeUnit.DAYS)
+        redisTemplate.opsForValue().set("refresh_token:$key", refreshToken, tokenProperties.refresh, TimeUnit.MILLISECONDS)
     }
 
     fun getRefreshToken(key: String): String? {
